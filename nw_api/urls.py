3982d4f9625279231df3ae39cfab6015dc2224e1
skiboorg <d@skib.org>
2021-10-05 20:09:36 +0300

    ads

--- nw_api/urls.py
+++ nw_api/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -21,5 +22,10 @@ urlpatterns = [
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
+
+    path(
+        "ads.txt",
+        TemplateView.as_view(template_name="file.txt", content_type="text/plain"),
+    ),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
